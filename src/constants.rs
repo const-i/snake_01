3fc80841a4d074c1b8f83384eeca9ba06a05f15d
Sea6 <srideepSsea6energy.com>
2020-05-07 21:43:50 +0530

    tuning parameters

--- src/constants.rs
+++ src/constants.rs
@@ -13,23 +13,23 @@ pub const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 // Neural Network Game
 pub const NUM_INDIVIDUALS: u32 = 1000;
 pub const NUM_GAMES_NN: u32 = 20;
-pub const NUM_GENERATIONS: u32 = 20;
+pub const NUM_GENERATIONS: u32 = 50;
 pub const NN_MAX_GAME_TIME: u32 = 100;
 
 // Q-Learing Game
-pub const NUM_GAMES_QL: u32 = 2000;
-pub const NUM_QLS: u32 = 4; // Generally multiple of number of cores
+pub const NUM_GAMES_QL: u32 = 2000; // Plateau after 2000 games
+pub const NUM_QLS: u32 = 4; // Should be a multiple of number of cores
 
 // Genetic Algorithm Properties
 pub const MUTATION_PROBABILITY: f64 = 0.005;
 pub const CROSSOVER_PROBABILITY: f64 = 0.01;
 
 // Game Render Properties
-pub const BLOCK_SIZE: u32 = 20;
+pub const BLOCK_SIZE: u32 = 30;
 pub const RENDER_UPS: u64 = 20;
 pub const RENDER_FPS_MAX: u64 = 20;
 
 // Q-Learning Properties
-pub const EPSILON_GREEDY: f64 = 0.01;
-pub const LEARNING_RATE: f64 = 0.1;
-pub const DISCOUNT_FACTOR: f64 = 0.9;
+pub const EPSILON_GREEDY: f64 = 0.0; // Looks like best results are with 0. Probably SARSA would de better here
+pub const LEARNING_RATE: f64 = 0.01; // Lower seems to be better, but too low gets worse
+pub const DISCOUNT_FACTOR: f64 = 0.9; // Seems to make not much difference
--- src/main.rs
+++ src/main.rs
@@ -26,23 +26,43 @@ fn main() {
     match game_type {
         GameType::Human => render_game(),
         GameType::GeneticAlgorithm => {
-            iterate_population(NUM_INDIVIDUALS, NUM_GAMES_NN, NUM_GENERATIONS, fitness_function)
+            iterate_population(NUM_INDIVIDUALS, NUM_GAMES_NN, NUM_GENERATIONS, fitness_function_nn)
         }
-        GameType::QLearning => iterate_qls(NUM_QLS, NUM_GAMES_QL, fitness_function),
+        GameType::QLearning => iterate_qls(NUM_QLS, NUM_GAMES_QL, fitness_function_ql),
     }
-
-    //render_game();
-    //iterate_population(NUM_INDIVIDUALS, NUM_GAMES_NN, NUM_GENERATIONS, fitness_function);
-    //iterate_qls(NUM_QLS, NUM_GAMES_QL, fitness_function);
 }
 
-fn fitness_function(_delta_t: i64, dist_before: i64, dist_after: i64, snake_eat: i64, snake_dead: i64) -> f64 {
+fn fitness_function_nn(_delta_t: i64, dist_before: i64, dist_after: i64, snake_eat: i64, snake_dead: i64) -> f64 {
     let mut fitness: f64 = 0.0_f64;
     // Distance
     if dist_after < dist_before {
         fitness += 0.3_f64;
     } else {
         fitness -= 0.5_f64;
+    }
+    if dist_before >= 2 && dist_after < 2 {
+        fitness += 0.5_f64;
+    }
+    // Time
+    fitness += 0.1_f64;
+    // Food
+    if snake_eat > 0 {
+        fitness += 5.0_f64;
+    }
+    // Dead
+    if snake_dead > 0 {
+        fitness -= 1.0_f64;
+    }
+    fitness
+}
+
+fn fitness_function_ql(_delta_t: i64, dist_before: i64, dist_after: i64, snake_eat: i64, snake_dead: i64) -> f64 {
+    let mut fitness: f64 = 0.0_f64;
+    // Distance
+    if dist_after < dist_before {
+        fitness += 0.3_f64;
+    } else {
+        fitness -= 0.7_f64;
     }
     if dist_before >= 2 && dist_after < 2 {
         fitness += 0.5_f64;
@@ -105,7 +125,7 @@ fn nn_play(nn: &NN, num_games: u32, fitness_function: fn(i64, i64, i64, i64, i64
         game.init();
         fitness += game.run_nn(nn, fitness_function);
     }
-    fitness
+    fitness / num_games as f64
 }
 
 // --------------------------------------------------------------------------------------
@@ -128,7 +148,7 @@ fn ql_play_parallel(
         .par_iter_mut()
         .map(|ql| ql_play(ql, num_games, fitness_function))
         .collect();
-    println!("Fitness: {:?}", fitness);
+    //println!("Fitness: {:?}", fitness);
     fitness
         .iter()
         .enumerate()
@@ -144,7 +164,8 @@ fn ql_play(ql: &mut QLearner, num_games: u32, fitness_function: fn(i64, i64, i64
         game.init();
         fitness += game.run_ql(ql, fitness_function);
     }
-    fitness
+    println!("Fitness: {:?}", fitness / num_games as f64);
+    fitness / num_games as f64
 }
 
 // --------------------------------------------------------------------------------------
